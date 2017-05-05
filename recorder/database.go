94a96547c93d49aadb75a7aa43f960b9647b130e
Hernan Cosi <hernan@hernancosi.com>
2017-05-04 18:12:08 -0700

    Add GetLatest database method. Stubs out most of CarInfo.

--- recorder/database.go
+++ recorder/database.go
@@ -3,6 +3,8 @@ package recorder
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/golang/glog"
 	_ "github.com/mattn/go-sqlite3"
@@ -49,6 +51,56 @@ func (db *CarDatabase) Close() {
 	db.conn.Close()
 }
 
+func (db *CarDatabase) GetLatest(ctx context.Context) (*CarInfo, error) {
+	glog.Infof("Querying database for latest record.")
+	q := "select timestamp, driving_state, latitude, longitude, speed, charging_state, battery_level from CARINFO order by timestamp desc limit 1"
+	rows, err := db.conn.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var carInfo *CarInfo
+	for rows.Next() {
+		if carInfo != nil {
+			// TODO: This is an assertion, but we should handle it more gracefully.
+			panic("Got more than one record for a 'limit 1' query: " + q)
+		}
+		var timestamp int64
+		var drivingState string
+		var lat float64
+		var lon float64
+		var speed int
+		var chargeState string
+		var battLevel int
+		err = rows.Scan(&timestamp, &drivingState, &lat, &lon, &speed, &chargeState, &battLevel)
+		if err != nil {
+			return nil, err
+		}
+		carInfo = &CarInfo{
+			Timestamp:    time.Unix(timestamp, 0),
+			Name:         "Eve", // TODO: Add to different table.
+			DrivingState: drivingState,
+			Position: CarPosition{
+				Latitude:  lat,
+				Longitude: lon,
+				Speed:     speed,
+			},
+			ChargingState: chargeState,
+			BatteryLevel:  battLevel,
+			Charge: &ChargeInfo{
+				Voltage:          0,
+				ActualCurrent:    0,
+				PilotCurrent:     0,
+				TimeToFullCharge: new(float64),
+			},
+		}
+	}
+	if carInfo == nil {
+		return nil, errors.New("Cannot find any records! Is the database empty?")
+	}
+	return carInfo, nil
+}
+
 func (db *CarDatabase) Insert(ctx context.Context, info *CarInfo) error {
 	tx, err := db.conn.Begin()
 	if err != nil {
@@ -78,5 +130,4 @@ func (db *CarDatabase) Insert(ctx context.Context, info *CarInfo) error {
 
 	glog.Infof("Saved record with timestamp %d into database.", info.Timestamp.Unix())
 	return nil
-
 }
