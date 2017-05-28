c9c53c739403b209ef52e26fac3718b55b630197
Hernan Cosi <hernan@hernancosi.com>
2017-05-28 00:45:49 -0700

    Add fuzzy rate limiter.

--- recorder/car_info_pub.go
+++ recorder/car_info_pub.go
@@ -10,7 +10,10 @@ import (
 )
 
 // TODO: Should be a flag
-const refreshDuration = 1 * time.Minute
+const drivingRefreshDuration = 15 * time.Second
+const normalRefreshDuration = 5 * time.Minute
+const chargingRefreshDuration = 1 * time.Minute
+const sleepingRefreshDuration = 15 * time.Minute
 
 type teslaPubHelper struct {
 	client *tesla.Client
@@ -40,7 +43,7 @@ func NewCarInfoPublisher(conf common.Configuration) (<-chan CarInfo, chan<- bool
 
 func (t *teslaPubHelper) updateIndefinitely(stop <-chan bool) {
 	var state *CarInfo = nil
-	refreshInfo := func() error {
+	doRefreshFn := func() error {
 		i, err := getCarInfo(t.client)
 		if err != nil {
 			return err
@@ -56,19 +59,64 @@ func (t *teslaPubHelper) updateIndefinitely(stop <-chan bool) {
 	retryStrategy := backoff.NewExponentialBackOff()
 	retryStrategy.MaxElapsedTime = 0
 	defer close(t.out)
+
+	limiter := newRateLimiter()
 	for {
 		// TODO: Allow cancelling of retry via context cancellation channel.
-		backoff.RetryNotify(refreshInfo, retryStrategy, onError)
+		backoff.RetryNotify(doRefreshFn, retryStrategy, onError)
 		glog.Info("Updated CarInfo")
 		select {
-		case t.out <- *state:
+		case t.out <- *state: // Send update to client.
 			break
-		case <-stop:
+		case <-stop: // Don't send further updates.
 			glog.Info("Tesla sampling stopped.")
 			return
-		default:
+		default: // The client wasn't ready for the update.
 		}
-		time.Sleep(refreshDuration)
+		limiter.RateLimit(state)
+	}
+}
+
+type rateLimiter struct {
+	ticker *time.Ticker
+}
+
+func newRateLimiter() rateLimiter {
+	return rateLimiter{
+		// Use a fast rate for the first tick.
+		ticker: time.NewTicker(drivingRefreshDuration),
+	}
+}
+
+func (rl *rateLimiter) RateLimit(carState *CarInfo) {
+	// Rate limit
+	<-rl.ticker.C
+	rl.ticker.Stop()
+
+	// Select a new ticker based on current state
+
+	// Fast ticking: car is being used.
+	if carState.DrivingState != "" {
+		rl.ticker = time.NewTicker(drivingRefreshDuration)
+		glog.Infof("Fast refreshing due to use: %s", drivingRefreshDuration)
+		return
+	}
+	// Normal ticking: car is charging
+	if carState.Charge != nil {
+		rl.ticker = time.NewTicker(chargingRefreshDuration)
+		glog.Infof("Refreshing due to charging: %s", chargingRefreshDuration)
+		return
 	}
 
+	// It's between midnight and 8 am and car isn't charging or being used.
+	// TODO: Change to car is not charging and it hasn't been used in 2 hours.
+	now := time.Now()
+	if now.Hour() >= 0 && now.Hour() <= 8 {
+		rl.ticker = time.NewTicker(sleepingRefreshDuration)
+		glog.Infof("Slow refreshing due to time of day (12 to 8 am): %s", sleepingRefreshDuration)
+		return
+
+	}
+	rl.ticker = time.NewTicker(normalRefreshDuration)
+	glog.Infof("Normal refreshing: %s", normalRefreshDuration)
 }
