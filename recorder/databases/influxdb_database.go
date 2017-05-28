9323daed7de71b5d33690df1d1f3d82b53ed6f00
Hernan Cosi <hernan@hernancosi.com>
2017-05-27 23:45:53 -0700

    Clean up commented out code

--- recorder/databases/influxdb_database.go
+++ recorder/databases/influxdb_database.go
@@ -100,32 +100,4 @@ func OpenInfluxDbDatabase(address string, username string, password string, data
 		conn:     c,
 		database: database,
 	}, nil
-	//
-	//	// Create a new point batch
-	//	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-	//		Database:  MyDB,
-	//		Precision: "s",
-	//	})
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//	// Create a point and add to batch
-	//	tags := map[string]string{"cpu": "cpu-total"}
-	//	fields := map[string]interface{}{
-	//		"idle":   10.1,
-	//		"system": 53.3,
-	//		"user":   46.6,
-	//	}
-	//
-	//	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//	bp.AddPoint(pt)
-	//
-	//	// Write the batch
-	//	if err := c.Write(bp); err != nil {
-	//		log.Fatal(err)
-	//	}
 }
